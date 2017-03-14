b0400eae8a717bd3003d9795b53c525cdb74aecb
MultiLi <litian9308@gmail.com>
2017-03-14 13:16:39 -0700

    ha

--- model.py
+++ model.py
@@ -30,11 +30,13 @@ class DCGan():
         while epoch < epochs:
             ind = np.random.permutation(data.shape[0])
             for i in range(batch_num):
+                sampledata = data[i * self.batch_size : (i+1)* self.batch_size]
                 for j in range(self.k):
                     z = np.random.rand(self.batch_size,self.z_dim) * 2 - 1
-                    sampledata = data[i * self.batch_size : (i+1)* self.batch_size]
                     _, dis_loss = self.sess.run([self.opt_dis,self.loss],
-                    feed_dict ={self.ginput: z, self.sampledata : sampledata, self.label : dis_label})
+                    feed_dict ={self.ginput: z,
+                                self.sampledata : sampledata,
+                                self.label : dis_label})
 
                 z = np.random.rand(self.batch_size,self.z_dim) * 2 - 1
                 _, gen_loss = self.sess.run([self.opt_gen,self.loss],
@@ -58,26 +60,26 @@ class DCGan():
                             initializer = tf.random_normal_initializer(0, self.std))
 
             self.fcg_b = tf.get_variable('fcg_b',[1024*4*4],initializer = tf.constant_initializer(0.0))
-            self.fcg_sc = tf.constant([1.0])#tf.get_variable('fcg_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.fcg_sh = tf.constant([0.0])#tf.get_variable('fcg_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.fcg_sc = tf.get_variable('fcg_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.fcg_sh = tf.get_variable('fcg_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.dconv1_w = tf.get_variable('dconv1_w',[5,5,512,1024],
                             initializer = tf.random_normal_initializer(0, self.std))
             self.dconv1_b = tf.get_variable('dconv1_b',[512],initializer = tf.constant_initializer(0.0))
-            self.dconv1_sc = tf.constant([1.0])#tf.get_variable('dconv1_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.dconv1_sh = tf.constant([0.0])#tf.get_variable('dconv1_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.dconv1_sc = tf.get_variable('dconv1_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.dconv1_sh = tf.get_variable('dconv1_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.dconv2_w = tf.get_variable('dconv2_w',[5,5,256,512],
                             initializer = tf.random_normal_initializer(0, self.std))
             self.dconv2_b = tf.get_variable('dconv2_b',[256],initializer = tf.constant_initializer(0.0))
-            self.dconv2_sc = tf.constant([1.0])#tf.get_variable('dconv2_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.dconv2_sh = tf.constant([0.0])#tf.get_variable('dconv2_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.dconv2_sc = tf.get_variable('dconv2_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.dconv2_sh = tf.get_variable('dconv2_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.dconv3_w = tf.get_variable('dconv3_w',[5,5,128,256],
                             initializer = tf.random_normal_initializer(0, self.std))
             self.dconv3_b = tf.get_variable('dconv3_b',[128],initializer = tf.constant_initializer(0.0))
-            self.dconv3_sc = tf.constant([1.0])#tf.get_variable('dconv3_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.dconv3_sh = tf.constant([0.0])#tf.get_variable('dconv3_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.dconv3_sc = tf.get_variable('dconv3_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.dconv3_sh = tf.get_variable('dconv3_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.dconv4_w = tf.get_variable('dconv4_w',[5,5,self.image_size[-1],128],
                             initializer = tf.random_normal_initializer(0, self.std))
@@ -88,26 +90,26 @@ class DCGan():
             self.conv1_w = tf.get_variable('conv1_w',[5,5,self.image_size[-1],64],
                             initializer = tf.random_normal_initializer(0, self.std))
             self.conv1_b = tf.get_variable('conv1_b',[64],initializer = tf.constant_initializer(0.0))
-            self.conv1_sc = tf.constant([1.0])#tf.get_variable('conv1_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.conv1_sh = tf.constant([0.0])#tf.get_variable('conv1_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.conv1_sc = tf.get_variable('conv1_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.conv1_sh = tf.get_variable('conv1_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.conv2_w = tf.get_variable('conv2_w',[5,5,64,128],
                             initializer = tf.random_normal_initializer(0, self.std))
             self.conv2_b = tf.get_variable('conv2_b',[128],initializer = tf.constant_initializer(0.0))
-            self.conv2_sc = tf.constant([1.0])#tf.get_variable('conv2_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.conv2_sh = tf.constant([0.0])#tf.get_variable('conv2_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.conv2_sc = tf.get_variable('conv2_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.conv2_sh = tf.get_variable('conv2_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.conv3_w = tf.get_variable('conv3_w',[5,5,128,256],
                             initializer = tf.random_normal_initializer(0, self.std))
             self.conv3_b = tf.get_variable('conv3_b',[256],initializer = tf.constant_initializer(0.0))
-            self.conv3_sc = tf.constant([1.0])#tf.get_variable('conv3_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.conv3_sh = tf.constant([0.0])#tf.get_variable('conv3_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.conv3_sc = tf.get_variable('conv3_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.conv3_sh = tf.get_variable('conv3_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.conv4_w = tf.get_variable('conv4_w',[5,5,256,512],
                             initializer = tf.random_normal_initializer(0, self.std))
             self.conv4_b = tf.get_variable('conv4_b',[512],initializer = tf.constant_initializer(0.0))
-            self.conv4_sc = tf.constant([1.0])#tf.get_variable('conv4_sc',[1],initializer = tf.constant_initializer(1.0))
-            self.conv4_sh = tf.constant([0.0])#tf.get_variable('conv4_sh',[1],initializer = tf.constant_initializer(0.0))
+            self.conv4_sc = tf.get_variable('conv4_sc',[1],initializer = tf.constant_initializer(1.0))
+            self.conv4_sh = tf.get_variable('conv4_sh',[1],initializer = tf.constant_initializer(0.0))
 
             self.fcd_w = tf.get_variable('fcd_w',[4*4*512,1],
                             initializer = tf.random_normal_initializer(0, self.std))
@@ -119,38 +121,38 @@ class DCGan():
         self.ginput = tf.placeholder(tf.float32, shape=(None,self.z_dim))
 
         with tf.variable_scope('gen_fc') as scope:
-            self.fcg = tf.nn.relu(tf.nn.bias_add(tf.matmul(self.ginput,self.fcg_w), self.fcg_b))
+            self.fcg = tf.nn.bias_add(tf.matmul(self.ginput,self.fcg_w), self.fcg_b)
             self.fcgo = tf.reshape(self.fcg,[-1,4,4,1024])
             self.fcgm, self.fcgv = tf.nn.moments(self.fcgo, [0,1,2])
-            self.fcgb = tf.nn.batch_normalization(self.fcgo, self.fcgm, self.fcgv,None, None,self.eps)
+            self.fcgb = tf.nn.relu(tf.nn.batch_normalization(self.fcgo, self.fcgm, self.fcgv,self.fcg_sh, self.fcg_sc,self.eps))
             # print self.dconv1_w.shape
 
         with tf.variable_scope('gen_dconv1') as scope:
-            self.dconv1 = tf.nn.relu( tf.nn.bias_add(tf.nn.conv2d_transpose(self.fcgb,
+            self.dconv1 = tf.nn.bias_add(tf.nn.conv2d_transpose(self.fcgb,
                                                               self.dconv1_w,
                                                               [self.batch_size,8,8,512],
-                                                              [1,2,2,1]),self.dconv1_b))
+                                                              [1,2,2,1]),self.dconv1_b)
             self.dconv1m, self.dconv1v = tf.nn.moments(self.dconv1, [0,1,2])
-            self.dconv1b = tf.nn.batch_normalization(self.dconv1, self.dconv1m, self.dconv1v,
-                                                     None, None,self.eps)
+            self.dconv1b = tf.nn.relu(tf.nn.batch_normalization(self.dconv1, self.dconv1m, self.dconv1v,
+                                                     self.dconv1_sh, self.dconv1_sc,self.eps))
 
         with tf.variable_scope('gen_dconv2') as scope:
-            self.dconv2 = tf.nn.relu( tf.nn.bias_add(tf.nn.conv2d_transpose(self.dconv1b,
+            self.dconv2 = tf.nn.bias_add(tf.nn.conv2d_transpose(self.dconv1b,
                                                               self.dconv2_w,
                                                               [self.batch_size, 16, 16, 256],
-                                                              [1,2,2,1]), self.dconv2_b))
+                                                              [1,2,2,1]), self.dconv2_b)
             self.dconv2m, self.dconv2v = tf.nn.moments(self.dconv2, [0,1,2])
-            self.dconv2b = tf.nn.batch_normalization(self.dconv2, self.dconv2m, self.dconv2v,
-                                                     None, None,self.eps)
+            self.dconv2b = tf.nn.relu(tf.nn.batch_normalization(self.dconv2, self.dconv2m, self.dconv2v,
+                                                     self.dconv2_sh, self.dconv2_sc,self.eps))
 
         with tf.variable_scope('gen_dconv3') as scope:
-            self.dconv3 = tf.nn.relu( tf.nn.bias_add(tf.nn.conv2d_transpose(self.dconv2b,
+            self.dconv3 = tf.nn.bias_add(tf.nn.conv2d_transpose(self.dconv2b,
                                                               self.dconv3_w,
                                                               [self.batch_size, 32, 32, 128],
-                                                              [1,2,2,1]), self.dconv3_b))
+                                                              [1,2,2,1]), self.dconv3_b)
             self.dconv3m, self.dconv3v = tf.nn.moments(self.dconv3, [0,1,2])
-            self.dconv3b = tf.nn.batch_normalization(self.dconv3, self.dconv3m, self.dconv3v,
-                                                     None, None,self.eps)
+            self.dconv3b = tf.nn.relu(tf.nn.batch_normalization(self.dconv3, self.dconv3m, self.dconv3v,
+                                                     self.dconv3_sh, self.dconv3_sc, self.eps))
 
         with tf.variable_scope('gen_dconv4') as scope:
             self.dconv4 = tf.nn.tanh(tf.nn.bias_add(tf.nn.conv2d_transpose(self.dconv3b,
@@ -165,34 +167,36 @@ class DCGan():
 
         with tf.variable_scope('dis_conv1') as scope:
             self.conv1 = tf.nn.bias_add(tf.nn.conv2d(self.dinput, self.conv1_w, [1,2,2,1], 'SAME'), self.conv1_b)
-            self.conv1r = 0.2 * self.conv1 + (1 - 0.2) * tf.nn.relu(self.conv1)
-            self.conv1m, self.conv1v = tf.nn.moments(self.conv1r, [0,1,2])
-            self.conv1b = tf.nn.batch_normalization(self.conv1r, self.conv1m, self.conv1v,
-                                                    None, None, self.eps
+            self.conv1m, self.conv1v = tf.nn.moments(self.conv1, [0,1,2])
+            self.conv1b = tf.nn.batch_normalization(self.conv1, self.conv1m, self.conv1v,
+                                                    self.conv1_sh, self.conv1_sc, self.eps)
+            self.conv1r = 0.2 * self.conv1b + (1 - 0.2) * tf.nn.relu(self.conv1b)
+
+
 
         with tf.variable_scope('dis_conv2') as scope:
-            self.conv2 = tf.nn.bias_add(tf.nn.conv2d(self.conv1b, self.conv2_w, [1,2,2,1], 'SAME') , self.conv2_b)
-            self.conv2r = 0.2 * self.conv2 + (1 - 0.2) * tf.nn.relu(self.conv2)
-            self.conv2m, self.conv2v = tf.nn.moments(self.conv2r, [0,1,2])
-            self.conv2b = tf.nn.batch_normalization(self.conv2r, self.conv2m, self.conv2v,
-                                                    None, None,self.eps)
+            self.conv2 = tf.nn.bias_add(tf.nn.conv2d(self.conv1r, self.conv2_w, [1,2,2,1], 'SAME') , self.conv2_b)
+            self.conv2m, self.conv2v = tf.nn.moments(self.conv2, [0,1,2])
+            self.conv2b = tf.nn.batch_normalization(self.conv2, self.conv2m, self.conv2v,
+                                                self.conv2_sh, self.conv2_sc,self.eps)
+            self.conv2r = 0.2 * self.conv2b + (1 - 0.2) * tf.nn.relu(self.conv2b)
 
         with tf.variable_scope('dis_conv3') as scope:
-            self.conv3 = tf.nn.bias_add(tf.nn.conv2d(self.conv2b, self.conv3_w, [1,2,2,1], 'SAME'), self.conv3_b)
-            self.conv3r = 0.2 * self.conv3 + (1 - 0.2) * tf.nn.relu(self.conv3)
-            self.conv3m, self.conv3v = tf.nn.moments(self.conv3r, [0,1,2])
-            self.conv3b = tf.nn.batch_normalization(self.conv3r, self.conv3m, self.conv3v,
-                                                    None, None, self.eps)
+            self.conv3 = tf.nn.bias_add(tf.nn.conv2d(self.conv2r, self.conv3_w, [1,2,2,1], 'SAME'), self.conv3_b)
+            self.conv3m, self.conv3v = tf.nn.moments(self.conv3, [0,1,2])
+            self.conv3b = tf.nn.batch_normalization(self.conv3, self.conv3m, self.conv3v,
+                                                    self.conv3_sh, self.conv3_sc, self.eps)
+            self.conv3r = 0.2 * self.conv3b + (1 - 0.2) * tf.nn.relu(self.conv3b)
 
         with tf.variable_scope('dis_conv4') as scope:
-            self.conv4 = tf.nn.bias_add(tf.nn.conv2d(self.conv3b, self.conv4_w, [1,2,2,1], 'SAME'), self.conv4_b)
-            self.conv4r = 0.2 * self.conv4 + (1 - 0.2) * tf.nn.relu(self.conv4)
-            self.conv4m, self.conv4v = tf.nn.moments(self.conv4r, [0,1,2])
-            self.conv4b = tf.nn.batch_normalization(self.conv4r, self.conv4m, self.conv4v,
-                                                    None, None, self.eps)
+            self.conv4 = tf.nn.bias_add(tf.nn.conv2d(self.conv3r, self.conv4_w, [1,2,2,1], 'SAME'), self.conv4_b)
+            self.conv4m, self.conv4v = tf.nn.moments(self.conv4, [0,1,2])
+            self.conv4b = tf.nn.batch_normalization(self.conv4, self.conv4m, self.conv4v,
+                                                    self.conv4_sh, self.conv4_sc, self.eps)
+            self.conv4r = 0.2 * self.conv4b + (1 - 0.2) * tf.nn.relu(self.conv4b)
 
         with tf.variable_scope('dis_fc') as scope:
-            self.fcd_in = tf.reshape(self.conv4b, [-1,4*4*512])
+            self.fcd_in = tf.reshape(self.conv4r, [-1,4*4*512])
             self.fcd = tf.nn.bias_add(tf.matmul(self.fcd_in, self.fcd_w), self.fcd_b)
 
         self.label = tf.placeholder(tf.float32, shape = (None))
